644cfc0c50b27ef21765fec95f5d268d491386d6
briannqc <brian.nqc@outlook.com>
2022-07-11 14:30:02 +0700

    Exercise 1.5 & 1.6: Lissajous

---
+++ ch1/sec4/lissajous.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var colorGreen = color.RGBA{0x0, 0xFF, 0x0, 0xFF}
+var colorRed = color.RGBA{0xFF, 0x0, 0x0, 0xFF}
+var colorBlue = color.RGBA{0x0, 0x0, 0xFF, 0xFF}
+var palatte = []color.Color{color.White, colorGreen, colorRed, colorBlue}
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // number of complete x oscillator revolutions
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms
+	)
+
+	freq := rand.Float64() * 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palatte)
+		colorIndex := rand.Intn(len(palatte)-1) + 1
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(colorIndex))
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim)
+}
