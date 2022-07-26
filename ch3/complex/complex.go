f581bb1929046cc4716940ffedf5bfe1d5d760c7
briannqc <brian.nqc@outlook.com>
2022-07-26 11:00:30 +0800

    Ex 3.7. Newton method

--- ch3/complex/complex.go
+++ ch3/complex/complex.go
@@ -20,7 +20,7 @@ func main() {
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+			img.Set(px, py, newton(z))
 		}
 	}
 	png.Encode(os.Stdout, img)
@@ -37,6 +37,18 @@ func mandelbrot(z complex128) color.Color {
 			blue := uint8(real(v)*128) + 127
 			red := uint8(imag(v)*128) + 127
 			return color.YCbCr{128, blue, red}
+		}
+	}
+	return color.Black
+}
+
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.Gray{255 - contrast*i}
 		}
 	}
 	return color.Black
