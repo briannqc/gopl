493de757d7d500634b8cff8eb12d48a60db99714
briannqc <brian.nqc@outlook.com>
2022-08-04 00:12:20 +0800

    Ex 4.2. Print SHA265, 384 and 512 checksum of inputs

---
+++ ch4/arrays/ex4.2.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var hash string
+	flag.StringVar(&hash, "hash", "SHA256", "Available options: SHA256 (default), SHA384, SHA512")
+
+	flag.Parse()
+	args := flag.Args()
+	fmt.Printf("Using %v checksum\n", hash)
+
+	checksumFn, ok := map[string]func([]byte) []byte{
+		"SHA256": func(b []byte) []byte {
+			cs := sha256.Sum256(b)
+			return cs[:]
+		},
+		"SHA384": func(b []byte) []byte {
+			cs := sha512.Sum384(b)
+			return cs[:]
+		},
+		"SHA512": func(b []byte) []byte {
+			cs := sha512.Sum512(b)
+			return cs[:]
+		},
+	}[hash]
+
+	if !ok {
+		fmt.Printf("Hash function: %v is not supported", hash)
+		os.Exit(1)
+	}
+
+	for _, arg := range args {
+		checksum := checksumFn([]byte(arg))
+		fmt.Printf("%x\t%s\n", checksum, arg)
+	}
+}
