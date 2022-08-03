7a1e578f1af9d31d6796961d434496725a27bceb
briannqc <brian.nqc@outlook.com>
2022-08-03 23:50:00 +0800

    Ex 4.1. Count bit diffs from 2 SHA256

---
+++ ch4/arrays/ex4.1.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	c1 := sha256.Sum256([]byte("x"))
+	c2 := sha256.Sum256([]byte("X"))
+
+	diff := uint(0)
+	for i := 0; i < len(c1); i++ {
+		b1, b2 := c1[i], c2[i]
+		diff += uint(pc[b1&b2])
+	}
+
+	fmt.Println("Bits diff:", diff)
+}
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
