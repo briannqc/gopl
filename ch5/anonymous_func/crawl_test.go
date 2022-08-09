ea2c053fed7adcc05455e8b0677c0d316e8f2a05
briannqc <brian.nqc@outlook.com>
2022-08-09 16:03:43 +0800

    Section 5.6. BFS web crawler

---
+++ ch5/anonymous_func/crawl_test.go
@@ -0,0 +1,84 @@
+package anonymousfunc
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+
+	"golang.org/x/net/html"
+)
+
+func bfs(f func(item string) []string, worklist []string) {
+	seen := map[string]bool{}
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := extractLinks(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
+
+func extractLinks(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return links, nil
+}
+
+func forEachNode(n *html.Node, pre, post func(*html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		post(n)
+	}
+}
+
+func TestBFSCrawl(t *testing.T) {
+	// panic: test timed out after 30s
+	bfs(crawl, []string{"https://golang.org"})
+}
