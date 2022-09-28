1cc0272529edc8191d7be0c064072b33ec0c0c2a
briannqc <brian.nqc@outlook.com>
2022-09-29 01:22:14 +0800

    Ex 9.6. Measure CPU bound program ex8.5 with GOMAXPROCS
    Exercise 9.6: Measure how the performance of a
    compute-bound parallel program (see Exercise 8.5)
    varies with GOMAXPROCS. What is the optimal value
    on your computer? How many CPUs does your computer
    have?
    
    Spec: 2.6 GHz 6-Core Intel Core i7

--- ch8/sec8.4/ex8.5/mandelbrot_test.go
+++ ch8/sec8.4/ex8.5/mandelbrot_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"runtime"
 	"testing"
 )
 
@@ -34,6 +35,81 @@ Benchmark_renderInParallel2
 Benchmark_renderInParallel2-12    	       3	 388499770 ns/op
 */
 func Benchmark_renderInParallel2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		renderInParallel2(io.Discard)
+	}
+}
+
+/*
+goos: darwin
+goarch: amd64
+pkg: github.com/briannqc/gopl/ch8/sec8.4/ex8.5
+cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+Benchmark_renderInParallel2_GOMAXPROCS_1
+Benchmark_renderInParallel2_GOMAXPROCS_1-12    	       1	1085448367 ns/op
+*/
+func Benchmark_renderInParallel2_GOMAXPROCS_1(b *testing.B) {
+	runtime.GOMAXPROCS(1)
+	for i := 0; i < b.N; i++ {
+		renderInParallel2(io.Discard)
+	}
+}
+
+/*
+goos: darwin
+goarch: amd64
+pkg: github.com/briannqc/gopl/ch8/sec8.4/ex8.5
+cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+Benchmark_renderInParallel2_GOMAXPROCS_2
+Benchmark_renderInParallel2_GOMAXPROCS_2-12    	       2	 524221212 ns/op
+*/
+func Benchmark_renderInParallel2_GOMAXPROCS_2(b *testing.B) {
+	runtime.GOMAXPROCS(2)
+	for i := 0; i < b.N; i++ {
+		renderInParallel2(io.Discard)
+	}
+}
+
+/*
+goos: darwin
+goarch: amd64
+pkg: github.com/briannqc/gopl/ch8/sec8.4/ex8.5
+cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+Benchmark_renderInParallel2_GOMAXPROCS_4
+Benchmark_renderInParallel2_GOMAXPROCS_4-12    	       3	 389211096 ns/op
+*/
+func Benchmark_renderInParallel2_GOMAXPROCS_4(b *testing.B) {
+	runtime.GOMAXPROCS(4)
+	for i := 0; i < b.N; i++ {
+		renderInParallel2(io.Discard)
+	}
+}
+
+/*
+goos: darwin
+goarch: amd64
+pkg: github.com/briannqc/gopl/ch8/sec8.4/ex8.5
+cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+Benchmark_renderInParallel2_GOMAXPROCS_8
+Benchmark_renderInParallel2_GOMAXPROCS_8-12    	       3	 381482230 ns/op
+*/
+func Benchmark_renderInParallel2_GOMAXPROCS_8(b *testing.B) {
+	runtime.GOMAXPROCS(8)
+	for i := 0; i < b.N; i++ {
+		renderInParallel2(io.Discard)
+	}
+}
+
+/*
+goos: darwin
+goarch: amd64
+pkg: github.com/briannqc/gopl/ch8/sec8.4/ex8.5
+cpu: Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz
+Benchmark_renderInParallel2_GOMAXPROCS_16
+Benchmark_renderInParallel2_GOMAXPROCS_16-12    	       3	 394391888 ns/op
+*/
+func Benchmark_renderInParallel2_GOMAXPROCS_16(b *testing.B) {
+	runtime.GOMAXPROCS(16)
 	for i := 0; i < b.N; i++ {
 		renderInParallel2(io.Discard)
 	}
