571cd1c85192d446b26a3830ada27b816bac2187
briannqc <brian.nqc@outlook.com>
2022-10-15 00:03:24 +0800

    Ex 11.4: Modify randomPalindrome to exercise IsPalindrome’s handling of punctuation and spaces

--- ch11/sec11.2/palindrome_test.go
+++ ch11/sec11.2/palindrome_test.go
@@ -37,10 +37,18 @@ func TestIsPalindrome(t *testing.T) {
 }
 
 func randomPalindrome(rng *rand.Rand) string {
-	n := rng.Intn(25)
+	n := rng.Intn(30)
 	runes := make([]rune, n)
 	for i := 0; i < (n+1)/2; i++ {
-		r := rune(rng.Intn(0x1000))
+		var r rune
+		if i%7 == 0 {
+			r = '.'
+		} else if i%13 == 0 {
+			r = ' '
+		} else {
+			r = rune(rng.Intn(0x1000))
+		}
+
 		runes[i] = r
 		runes[n-1-i] = r
 	}
